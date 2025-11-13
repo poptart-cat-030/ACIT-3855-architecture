284704dcc55d674473ea841dc4a858475ba86d53
poptart-cat-030 <hillarylam030@gmail.com>
2025-11-13 10:40:54 -0800

    Put log files for each service in logs folder and log mysql connection string

--- storage/create_database.py
+++ storage/create_database.py
@@ -1,12 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-import yaml # For using the yaml config file (app_conf)
+# For creating and displaying log messages (log_conf)
+import logging
+import logging.config
+
+import yaml # For using the yaml config files (app_conf and log_conf)
 
 with open('config/app_conf.yaml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-ENGINE = create_engine(f"mysql+pymysql://{app_config['datastore']['user']}:{app_config['datastore']['password']}@{app_config['datastore']['hostname']}:{app_config['datastore']['port']}/{app_config['datastore']['db']}")
+# Logging configurations
+with open("config/log_conf.yaml", "r") as f:
+    LOG_CONFIG = yaml.safe_load(f.read())
+    logging.config.dictConfig(LOG_CONFIG)
+
+logger = logging.getLogger('basicLogger')
+
+logger.info(f"MySQL connection string: 'mysql+pymysql://{app_config['datastore']['user']}:{app_config['datastore']['password']}@{app_config['datastore']['hostname']}:{app_config['datastore']['port']}/{app_config['datastore']['db']}'")
+
+ENGINE = create_engine(f"mysql+pymysql://{app_config['datastore']['user']}:{app_config['datastore']['password']}@{app_config['datastore']['hostname']}:{app_config['datastore']['port']}/{app_config['datastore']['db']}", pool_recycle=360)
 
 
 def make_session():
